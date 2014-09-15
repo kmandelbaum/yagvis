23a75dcd64aec139f2efef938bcc2c900096b575
Kontantin Mandelbaum <submultiplicativity@gmail.com>
2014-09-15 16:57:33 -0700

    [DEVEL] Bind data for visualization

--- src/GVis/GVis.hs
+++ src/GVis/GVis.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE TypeFamilies,TypeSynonymInstances,RankNTypes,FlexibleContexts #-}
-module GVis.GVis where 
+module GVis.GVis where
 
 import Data.Graph.Inductive hiding ( (&), Path )
-import Data.Graph.Inductive.Basic
 --import Data.Graph.Inductive.Graph
 import Diagrams.Prelude
-import Data.GraphViz (dotToGraph, Attributes)
-import Data.Maybe
-import Diagrams.TwoD.Text as DiaText
+import Control.Lens( _1, traverse, over )
+import GVis.GraphAlgo
+import Data.Array
 
-hvcat' ::  ( Monoid a, 
+hvcat' ::  ( Monoid a,
              HasOrigin a,
              Juxtaposable a,
              Semigroup a,
@@ -18,3 +17,29 @@ hvcat' ::  ( Monoid a,
     CatOpts R2 -> CatOpts R2 -> [[a]] -> a
 hvcat' hCatOpts vCatOpts xs = vcat' vCatOpts $ map ( \x -> hcat' hCatOpts x # center ) xs
 
+--internal representation data type for visualizable graphs
+--
+--
+
+data Shape = Rectangle | Ellipse
+
+data GVRepNode a = GVRepNode { isVisible :: Bool,
+                               nodeData :: Maybe a,
+                               gvLevel :: Int }
+
+data GVRepEdge a = GVRepEdge { isPartial :: Bool,
+                               edgeData :: Maybe a }
+
+type GVRepGraph gr a b = DynGraph gr => gr (GVRepNode a) (GVRepEdge b)
+
+convertGraph :: forall a b gr . DynGraph gr => gr a b -> GVRepGraph gr a b
+convertGraph g = insertPseudo $ gmap ( \(p, n, l, s) -> ( bindEdges p, n, bindVertex l n, bindEdges s ) ) g
+  where
+    bindVertex x n = GVRepNode{ isVisible = True, nodeData = Just x, gvLevel = undefined }
+    bindEdge x = GVRepEdge{ isPartial = False, edgeData = Just x }
+    bindEdges = over (traverse . _1) bindEdge
+    allEdges = labEdges g
+    insertPseudo :: GVRepGraph gr a b -> GVRepGraph gr a b
+    insertPseudo g' = foldr (undefined) g' $ filter isCrossLevel allEdges
+    isCrossLevel :: LEdge b -> Bool
+    isCrossLevel = undefined
